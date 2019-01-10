013c6fe1daa2d4d816c951313042218eb2730d89
konfara <konfar.andras@gmail.com>
2019-01-10 16:15:57 +0100

    test

---
+++ force-app/main/default/aura/andrisTestApp/andrisTestApp.app
@@ -0,0 +1,7 @@
+<aura:application extends="force:slds">
+
+AndrisTestApp replace
+
+    <c:tcListCases/>
+
+</aura:application>	
--- installpackage.sh
+++ installpackage.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -x
+set -e
 DESCRIPTION=testdescription
-DEVHUB=DevHub
-PACKAGENAME=testpackage
-TARGET=MyTP
+DEVHUB=TDevHub
+PACKAGENAME=teltestpackage
+TARGET=telkonfara
 KEY=keyunicon19
 
 echo starting installation of package...
 
 #sfdx force:package:create --name $PACKAGENAME --description $DESCRIPTION --packagetype Unlocked --path force-app --nonamespace --targetdevhubusername $DEVHUB
 
-sfdx force:package:version:create -p $PACKAGENAME -d force-app -k $KEY --wait 10 -v $DEVHUB
+sfdx force:package:version:create -p $PACKAGENAME -k $KEY --wait 10 -v $DEVHUB
 
 PACKAGES=`jq '.packageAliases' sfdx-project.json | jq 'keys'`
 LASTPACKAGE=`echo $PACKAGES | jq length`
@@ -18,5 +19,6 @@ LASTPACKAGE=$(($LASTPACKAGE-1))
 VERSION=`echo $PACKAGES | jq ".[$LASTPACKAGE]"`
 VERSION=${VERSION//\"}
 
+sfdx force:package:version:promote -n -p $VERSION
 sfdx force:package:install --wait 10 --publishwait 10 --package $VERSION -k $KEY -r -u $TARGET
 
